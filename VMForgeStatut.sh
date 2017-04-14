dee4125073f6d4e4ea011e059fcad17c12875f9c
yousrael <yousraelghzizal@hotmail.fr>
2017-04-14 16:01:18 +0200

    Update VMForgeStatut.sh

--- VMForgeStatut.sh
+++ VMForgeStatut.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-echo '{'
+_getRempliHome()
+{
+	  df -h /home/jenkins | grep -Eo '[0-9]{1,3}%' 
+  }
+  _getRempligitlab()
+  {
+	    df -h /gitlab | grep -Eo '[0-9]{1,3}%' 
+    }
+    _getMemoire()
+    {
+	     free -m | grep 'Mem' | tr -s " " | cut -d" " -f 3
+	      }
+	        
+	     
+		echo '{'
 			  echo '"group": "#Home",'
+			    echo '"icon": "fa fa-home",'
 			     echo  '"subgroups": ['
 			       echo  '{'
 					    echo '"title": "Hardware",'
+					         echo '"icon": "fa fa-cog",'
 						    echo ' "content": ['
-						          echo '{"type": "plain","text": "Remplissage Gitlab", "value": "ok"},'
-								         echo '{"type": "plain","text": "Remplissage Home", "color":"blue","value": "coucou"},'
-										 	 echo '{"type": "plain", "text": "Memoire vive", "color":"blue","value": "hello","endline": ""}'
+						          echo '{"type": "plain", "icon": "fa fa-hdd-o", "text": "Remplissage Gitlab", "value": "$(_getRempligitlab)  "},'
+								         echo '{"type": "plain", "icon": "fa fa-hdd-o", "text": "Remplissage Home", "color":"blue","value": "$(_getRempliHome)  "},'
+										 	 echo '{"type": "plain", "icon": "fa fa-hdd-o", "text": "Memoire vive", "color":"blue","value": "$(_getMemoire)","endline": ""}'
 											      echo ']'
 											        echo '}'
 												  echo ']'
 											  echo '}'
+											
+											  
