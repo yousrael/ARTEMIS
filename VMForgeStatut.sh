cea9f4b003526e924b375b15dec1f234c62e6967
yousrael <yousraelghzizal@hotmail.fr>
2017-04-14 15:43:29 +0200

    update

--- VMForgeStatut.sh
+++ VMForgeStatut.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
-_getRempliHome()
-{
-	  df -h /home/jenkins | grep -Eo '[0-9]{1,3}%' 
-  }
-  _getRempligitlab()
-  {
-	    df -h /gitlab | grep -Eo '[0-9]{1,3}%' 
-    }
-    _getMemoire()
-    {
-	     free -m | grep 'Mem' | tr -s " " | cut -d" " -f 3
-	      }
-	        
-	         cat <<EOF
-		 {
-			   "group": "#Home",
-			     "icon": "fa fa-home",
-			       "subgroups": [
-			         {
-					     "title": "Hardware",
-					         "icon": "fa fa-cog",
-						     "content": [
-						           {"type": "plain", "icon": "fa fa-hdd-o", "text": "Remplissage Gitlab", "value": "$(_getRempligitlab)  "},
-								         {"type": "plain", "icon": "fa fa-hdd-o", "text": "Remplissage Home", "color":"blue","value": "$(_getRempliHome)  "},
-										 	  {"type": "plain", "icon": "fa fa-hdd-o", "text": "Memoire vive", "color":"blue","value": "$(_getMemoire)","endline": ""}
-											      ]
-											        }
-												  ]
-											  }
-											  EOF
+echo '{'
+			  echo '"group": "#Home",'
+			     echo  '"subgroups": ['
+			       echo  '{'
+					    echo '"title": "Hardware",'
+						    echo ' "content": ['
+						          echo '{"type": "plain","text": "Remplissage Gitlab", "value": "ok"},'
+								         echo '{"type": "plain","text": "Remplissage Home", "color":"blue","value": "coucou"},'
+										 	 echo '{"type": "plain", "text": "Memoire vive", "color":"blue","value": "hello","endline": ""}'
+											      echo ']'
+											        echo '}'
+												  echo ']'
+											  echo '}'
